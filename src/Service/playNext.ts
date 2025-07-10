bc2e67bf2b621dca711aa95f4cde5e63f90abb88
iamayushmishtaiitr <iamayushmishra1510@gmail.com>
2025-07-11 02:30:07 +0530

    changed cors origin

--- src/Service/playNext.ts
+++ src/Service/playNext.ts
@@ -12,14 +12,16 @@ const playNext = async (req: Request, res: Response) => {
     });
 
     if (space?.hostId !== data.userId) {
-      return res.status(403).json({ message: "Only host can change current stream." });
+      return res
+        .status(403)
+        .json({ message: "Only host can change current stream." });
     }
 
     const mostUpvotedStream = await db.stream.findFirst({
       where: {
         userId: data.userId,
         spaceId: data.spaceId,
-        active :true
+        active: true,
       },
       orderBy: {
         upvotes: {
@@ -27,46 +29,44 @@ const playNext = async (req: Request, res: Response) => {
         },
       },
     });
-   
+
     if (!mostUpvotedStream) {
-      return res.status(404).json({ message: "No upvoted streams found to play next." });
+      return res
+        .status(404)
+        .json({ message: "No upvoted streams found to play next." });
     }
 
     await Promise.all([
       db.currentStream.upsert({
-      where: {
-        userId_spaceId: {
+        where: {
+          userId_spaceId: {
+            userId: data.userId,
+            spaceId: data.spaceId,
+          },
+        },
+        update: {
+          streamId: mostUpvotedStream.id,
+        },
+        create: {
           userId: data.userId,
           spaceId: data.spaceId,
+          streamId: mostUpvotedStream.id,
         },
-      },
-      update: {
-        streamId: mostUpvotedStream.id,
-      },
-      create: {
-        userId: data.userId,
-        spaceId: data.spaceId,
-        streamId: mostUpvotedStream.id,
-      },
-    }  ,
-  )  ,
-  db.stream.update({
-    where :{
-      id : mostUpvotedStream.id
-    } ,
-    data :{
-      active : false
-    }
-  })
+      }),
+      db.stream.update({
+        where: {
+          id: mostUpvotedStream.id,
+        },
+        data: {
+          active: false,
+        },
+      }),
+    ]);
 
-
-  ]);
-   
-    return res.status(200).json({ 
-      message: "Updated the current stream of the space", 
-      streamId: mostUpvotedStream.id 
+    return res.status(200).json({
+      message: "Updated the current stream of the space",
+      streamId: mostUpvotedStream.id,
     });
-
   } catch (error) {
     console.error("playNext error:", error);
     return res.status(500).json({ message: "Something went wrong" });
--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,31 @@
-import express, { Request, Response } from "express"
-import cors from 'cors';
-import streamsController from "./Controller/streamsController"
-import signUp from "./Controller/signUp"
-import signIn from "./Controller/signIn"
-import spaceController from "./Controller/spaceController"
-import cookieParser from 'cookie-parser';
-import dotenv from "dotenv"
+import express, { Request, Response } from "express";
+import cors from "cors";
+import streamsController from "./Controller/streamsController";
+import signUp from "./Controller/signUp";
+import signIn from "./Controller/signIn";
+import spaceController from "./Controller/spaceController";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
 
-dotenv.config() ;
-const app =express()
-app.use(cors()); 
+dotenv.config();
+const app = express();
+
+app.use(
+  cors({
+    origin: "https://music-frontend-yymr.vercel.app",
+    credentials: true, 
+  })
+);
+
 app.use(express.json());
 app.use(cookieParser());
 app.get("/health", (req: Request, res: Response) => {
   res.status(200).send("The Server is Working");
 });
-app.use("/signup",signUp)
-app.use("/signin",signIn)
-app.use("/space", spaceController)
-app.use("/stream", streamsController); 
+app.use("/signup", signUp);
+app.use("/signin", signIn);
+app.use("/space", spaceController);
+app.use("/stream", streamsController);
 app.listen(process.env.PORT, () => {
-    console.log(`Server running  on port ${process.env.PORT} `);
-  })
+  console.log(`Server running  on port ${process.env.PORT} `);
+});
