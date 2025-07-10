f1a8099773a1ab047ce3b033d405e5ae4e8c09f1
iamayushmishtaiitr <iamayushmishra1510@gmail.com>
2025-07-11 01:49:29 +0530

    chnaged index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express from "express"
+import express, { Request, Response } from "express"
 import cors from 'cors';
 import streamsController from "./Controller/streamsController"
 import signUp from "./Controller/signUp"
@@ -12,6 +12,9 @@ const app =express()
 app.use(cors()); 
 app.use(express.json());
 app.use(cookieParser());
+app.get("/health", (req: Request, res: Response) => {
+  res.status(200).send("The Server is Working");
+});
 app.use("/signup",signUp)
 app.use("/signin",signIn)
 app.use("/space", spaceController)
@@ -19,4 +22,3 @@ app.use("/stream", streamsController);
 app.listen(process.env.PORT, () => {
     console.log(`Server running  on port ${process.env.PORT} `);
   })
-  export default app;
