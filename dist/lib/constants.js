ff83ad68f47825e31198826e3c3a410a71cb9b63
iamayushmishtaiitr <iamayushmishra1510@gmail.com>
2025-07-11 14:31:45 +0530

    removed dist

--- dist/lib/constants.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SPT_REGEX = exports.YT_REGX = void 0;
-exports.YT_REGX = /(?:youtube\.com\/watch\?v=|youtu\.be\/|youtube\.com\/embed\/)([^&\n?#]+)/;
-exports.SPT_REGEX = /^(https?:\/\/)?(open\.)?spotify\.com\/(track|playlist|album|artist)\/[a-zA-Z0-9]+$/i;
